995ac0ee5b79ff91f9027df01e79559e59761dd6
Huseyin ELMAS <hsynlms47@gmail.com>
2020-11-01 21:33:48 +0300

    feat: new type casting helper

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,35 @@
 
 const helpers = {
   /**
+   * Casts the primitive value to the automatically detected type
+   * @param value The primitive value that will be casted
+   * @returns Auto casted value
+   */
+  typeCast: value => {
+    // return not supported type values
+    if (
+      typeof value !== 'number' &&
+      typeof value !== 'boolean' &&
+      typeof value !== 'string'
+    ) return value
+
+    // validation
+    if (typeof value === 'string' && !value.trim()) {
+      return value
+    }
+
+    // auto-casting
+    if (/^\d+$/.test(value)) {
+      return parseInt(value, 10)
+    } else if (/^\d+\.\d+$/.test(value)) {
+      return parseFloat(value)
+    } else if (/^(true|false)$/.test(value)) {
+      return value === 'true'
+    }
+
+    return value.toString()
+  },
+  /**
    * Returns string literal level escaped RegExp string
    * @param {String} shortCodesArr Name of all the shortcode tags intended to be parsed
    * @returns {String} Escaped string literal level RegExp string
