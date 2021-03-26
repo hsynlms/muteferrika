e7a948b6fd13adbb8bcbe92abe4702a1ffafb245
Huseyin ELMAS <hsynlms47@gmail.com>
2021-03-26 22:28:41 +0300

    chore: general tweaks about indentations, readability, naming etc.

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const helpers = {
-  /**
-   * Casts the primitive value to the automatically detected type
-   * @param {string} value The primitive value that will be casted
-   * @returns {(string|boolean|number)} Auto casted value
-   */
-  typeCast: value => {
+/**
+ * Casts the primitive value to the automatically detected type
+ * @param {string} value The primitive value that will be casted
+ * @returns {(string|boolean|number)} Auto casted value
+ */
+exports.typeCast =
+  value => {
     // return not supported type values
     if (
       typeof value !== 'number' &&
@@ -29,23 +29,27 @@ const helpers = {
     }
 
     return value.toString()
-  },
-  /**
-   * Returns string literal level escaped RegExp string
-   * @param {string} string String literal level RegExp string
-   * @returns {string} Escaped string literal level RegExp string
-   */
-  escapeRegExp: string => {
+  }
+
+/**
+ * Returns string literal level escaped RegExp string
+ * @param {string} string String literal level RegExp string
+ * @returns {string} Escaped string literal level RegExp string
+ */
+exports.escapeRegExp =
+  string => {
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
     return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&')
-  },
-  /**
-   * Returns shortcode tag parser regexp object
-   * @param {array} shortCodesArr Name of all the shortcode tags intended to be parsed
-   * @param {string} flags RegExp flags
-   * @returns {RegExp} A Regexp object instance
-   */
-  getShortCodeTagRegex: (shortCodesArr, flags = '') => {
+  }
+
+/**
+ * Returns shortcode tag parser regexp object
+ * @param {array} shortCodesArr Name of all the shortcode tags intended to be parsed
+ * @param {string} flags RegExp flags
+ * @returns {RegExp} A Regexp object instance
+ */
+exports.getShortCodeTagRegexp =
+  (shortCodesArr, flags = '') => {
     // https://core.trac.wordpress.org/browser/tags/5.5.2/src/wp-includes/shortcodes.php#L226
     // eslint-disable-next-line
     const regexps = [/\[(\[?)/, /(?![\w-])([^\]\/]*(?:\/(?!\])[^\]\/]*)*?)(?:(\/)\]|\](?:([^\[]*(?:\[(?!\/\2\])[^\[]*)*)\[\/\2\])?)(\]?)/]
@@ -54,13 +58,15 @@ const helpers = {
       `${regexps[0].source}(${shortCodesArr.join('|')})${regexps[1].source}`,
       flags
     )
-  },
-  /**
-   * Returns shortcode tag attribute parser regexp object
-   * @param {string} flags RegExp flags
-   * @returns {RegExp} A Regexp object
-   */
-  getShortCodeAttrRegexp: (flags = '') => {
+  }
+
+/**
+ * Returns shortcode tag attribute parser regexp object
+ * @param {string} flags RegExp flags
+ * @returns {RegExp} A Regexp object
+ */
+exports.getShortCodeAttrRegexp =
+  (flags = '') => {
     // https://core.trac.wordpress.org/browser/tags/5.5.2/src/wp-includes/shortcodes.php#L499
     const regexp = /([\w-]+)\s*=\s*"([^"]*)"(?:\s|$)|([\w-]+)\s*=\s*'([^']*)'(?:\s|$)|([\w-]+)\s*=\s*([^\s'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|'([^']*)'(?:\s|$)|(\S+)(?:\s|$)/
 
@@ -69,6 +75,3 @@ const helpers = {
       flags
     )
   }
-}
-
-module.exports = helpers
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 'use strict'
 
-// get required node modules
-const helpers = require('./helpers')
+const {
+  getShortCodeTagRegexp,
+  getShortCodeAttrRegexp,
+  escapeRegExp,
+  typeCast
+} = require('./helpers')
 
 /**
  * Muteferrika class
@@ -11,10 +15,11 @@ function Muteferrika () {
 }
 
 // #region Private Method(s) of Muteferrika
+
 function _fetchTags (content) {
   // parse shortcode tags
   const tagNames = this._shortcodes.map(x => x.name)
-  const tagRegexp = helpers.getShortCodeTagRegex(tagNames, 'gmi')
+  const tagRegexp = getShortCodeTagRegexp(tagNames, 'gmi')
   const tagMatches = [...RegExp.prototype[Symbol.matchAll].call(tagRegexp, content)]
 
   return {
@@ -31,7 +36,7 @@ function _tagParser (tag, output) {
     // make the shortcode not getting rendered
     return output.replace(
       new RegExp(
-        helpers.escapeRegExp(tag[0].trim()),
+        escapeRegExp(tag[0].trim()),
         'gmi'
       ),
       tag[0].trim()
@@ -61,7 +66,7 @@ function _tagParser (tag, output) {
 
 function _fetchTagAttributes (rawAttributes) {
   // get shortcode tag parser regexp
-  const tagAttrRegexp = helpers.getShortCodeAttrRegexp('gmi')
+  const tagAttrRegexp = getShortCodeAttrRegexp('gmi')
 
   // reveal the tag attributes
   const tagAttrMatches =
@@ -72,7 +77,7 @@ function _fetchTagAttributes (rawAttributes) {
       .filter(attr => typeof attr[1] !== 'undefined' && typeof attr[2] !== 'undefined')
       .reduce(
         (acc, attr) => {
-          acc[attr[1].trim()] = helpers.typeCast(attr[2].trim())
+          acc[attr[1].trim()] = typeCast(attr[2].trim())
           return acc
         },
         {}
@@ -104,64 +109,21 @@ function _prepFinalOutput (fullMatch, shortcodeOutput, finalOutput) {
  * @param {string} name Name of the shortcode
  * @param {function} callback Shortcode renderer callback function
  */
-Muteferrika.prototype.add = function (name, callback) {
-  // validation
-  if (typeof name !== 'string') {
-    throw new TypeError(`"name" is expected to be a string but got: ${typeof name}`)
-  }
-
-  // remove space from start and end of the shortcode name
-  const _name = name.trim()
-
-  // validations
-  if (!_name) throw new TypeError('"name" cannot be empty')
-  if (typeof callback !== 'function') {
-    throw new TypeError(`"callback" is expected to be a function but got: ${typeof callback}`)
-  }
-
-  // check the existence of the given shortcode
-  if (
-    this._shortcodes
-      .filter(x => x.name === _name)
-      .length
-  ) {
-    throw new Error(`A same name shortcode does already exist: ${_name}`)
-  }
-
-  // add shortcode to the list
-  this._shortcodes =
-    this._shortcodes.concat({
-      name: _name,
-      callback
-    })
-}
-
-/**
- * Adds given shortcodes to the shortcode list to be used in rendering process
- * @param {array} shortcodes List of the shortcodes
- */
-Muteferrika.prototype.addRange = function (shortcodes) {
-  // validation
-  if (!Array.isArray(shortcodes)) {
-    throw new TypeError(`"shortcodes" is expected to be an array but got: ${typeof name}`)
-  }
-
-  // loop shortcodes
-  shortcodes.forEach(shortcode => {
-    // validate shortcode fields
-    if (!('name' in shortcode) ||
-        typeof shortcode.name !== 'string' ||
-        !shortcode.name.trim()) {
-      throw new TypeError('"name" may not be provided, may not in string type or may be empty')
-    }
-
-    if (!('callback' in shortcode) ||
-        typeof shortcode.callback !== 'function') {
-      throw new TypeError('"callback" may not be provided or may not be a function')
+Muteferrika.prototype.add =
+  function (name, callback) {
+    // validation
+    if (typeof name !== 'string') {
+      throw new TypeError(`"name" is expected to be a string but got: ${typeof name}`)
     }
 
     // remove space from start and end of the shortcode name
-    const _name = shortcode.name.trim()
+    const _name = name.trim()
+
+    // validations
+    if (!_name) throw new TypeError('"name" cannot be empty')
+    if (typeof callback !== 'function') {
+      throw new TypeError(`"callback" is expected to be a function but got: ${typeof callback}`)
+    }
 
     // check the existence of the given shortcode
     if (
@@ -176,39 +138,86 @@ Muteferrika.prototype.addRange = function (shortcodes) {
     this._shortcodes =
       this._shortcodes.concat({
         name: _name,
-        callback: shortcode.callback
+        callback
       })
-  })
-}
+  }
+
+/**
+ * Adds given shortcodes to the shortcode list to be used in rendering process
+ * @param {array} shortcodes List of the shortcodes
+ */
+Muteferrika.prototype.addRange =
+  function (shortcodes) {
+    // validation
+    if (!Array.isArray(shortcodes)) {
+      throw new TypeError(`"shortcodes" is expected to be an array but got: ${typeof name}`)
+    }
+
+    // loop shortcodes
+    shortcodes.forEach(shortcode => {
+      // validate shortcode fields
+      if (!('name' in shortcode) ||
+          typeof shortcode.name !== 'string' ||
+          !shortcode.name.trim()) {
+        throw new TypeError('"name" may not be provided, may not in string type or may be empty')
+      }
+
+      if (!('callback' in shortcode) ||
+          typeof shortcode.callback !== 'function') {
+        throw new TypeError('"callback" may not be provided or may not be a function')
+      }
+
+      // remove space from start and end of the shortcode name
+      const _name = shortcode.name.trim()
+
+      // check the existence of the given shortcode
+      if (
+        this._shortcodes
+          .filter(x => x.name === _name)
+          .length
+      ) {
+        throw new Error(`A same name shortcode does already exist: ${_name}`)
+      }
+
+      // add shortcode to the list
+      this._shortcodes =
+        this._shortcodes.concat({
+          name: _name,
+          callback: shortcode.callback
+        })
+    })
+  }
 
 /**
  * Removes given shortcode from the shortcode list
  * @param {string} name Name of the shortcode
  */
-Muteferrika.prototype.remove = function (name) {
-  // validation
-  if (typeof name !== 'string') {
-    throw new TypeError(`"name" is expected to be a string but got: ${typeof name}`)
+Muteferrika.prototype.remove =
+  function (name) {
+    // validation
+    if (typeof name !== 'string') {
+      throw new TypeError(`"name" is expected to be a string but got: ${typeof name}`)
+    }
+
+    // remove space from start and end of the shortcode name
+    const _name = name.trim()
+
+    // validation
+    if (!_name) throw new TypeError('"name" cannot be empty')
+
+    // remove the shortcode from list
+    this._shortcodes =
+      this._shortcodes.filter(x => x.name !== _name)
   }
-
-  // remove space from start and end of the shortcode name
-  const _name = name.trim()
-
-  // validation
-  if (!_name) throw new TypeError('"name" cannot be empty')
-
-  // remove the shortcode from list
-  this._shortcodes =
-    this._shortcodes.filter(x => x.name !== _name)
-}
 
 /**
  * Clears shortcode list
  */
-Muteferrika.prototype.clear = function () {
-  // clear the shortcode list
-  this._shortcodes = []
-}
+Muteferrika.prototype.clear =
+  function () {
+    // clear the shortcode list
+    this._shortcodes = []
+  }
 
 /**
  * Overrides callback function of the given shortcode
@@ -216,51 +225,53 @@ Muteferrika.prototype.clear = function () {
  * @param {function} callback New shortcode renderer callback function
  * @returns {boolean} Indicates success of the overriding process
  */
-Muteferrika.prototype.override = function (name, callback) {
-  // validation
-  if (typeof name !== 'string') {
-    throw new TypeError(`"name" is expected to be a string but got: ${typeof name}`)
+Muteferrika.prototype.override =
+  function (name, callback) {
+    // validation
+    if (typeof name !== 'string') {
+      throw new TypeError(`"name" is expected to be a string but got: ${typeof name}`)
+    }
+
+    // remove space chars from start and end of the shortcode name
+    const _name = name.trim()
+
+    // validation
+    if (!_name) throw new TypeError('"name" cannot be empty')
+    if (typeof callback !== 'function') {
+      throw new TypeError(`"callback" is expected to be a function but got: ${typeof callback}`)
+    }
+
+    // get the shortcode
+    const shortcode =
+      this._shortcodes.filter(x => x.name === _name)
+
+    if (!shortcode.length) return false
+
+    // override the callback function
+    shortcode[0].callback = callback
+
+    return true
   }
-
-  // remove space chars from start and end of the shortcode name
-  const _name = name.trim()
-
-  // validation
-  if (!_name) throw new TypeError('"name" cannot be empty')
-  if (typeof callback !== 'function') {
-    throw new TypeError(`"callback" is expected to be a function but got: ${typeof callback}`)
-  }
-
-  // get the shortcode
-  const shortcode =
-    this._shortcodes.filter(x => x.name === _name)
-
-  if (!shortcode.length) return false
-
-  // override the callback function
-  shortcode[0].callback = callback
-
-  return true
-}
 
 /**
  * Returns defined shortcodes
  * @returns {array} List of shortcodes
  */
-Muteferrika.prototype.shortcodes = function () {
-  let mutatedList = []
+Muteferrika.prototype.shortcodes =
+  function () {
+    let mutatedList = []
 
-  // loop all shortcodes
-  this._shortcodes.forEach(x => {
-    mutatedList =
-      mutatedList.concat(
-        Object.assign({}, x)
-      )
-  })
+    // loop all shortcodes
+    this._shortcodes.forEach(x => {
+      mutatedList =
+        mutatedList.concat(
+          Object.assign({}, x)
+        )
+    })
 
-  // return the mutated list
-  return mutatedList
-}
+    // return the mutated list
+    return mutatedList
+  }
 
 /**
  * Returns rendered output content
@@ -268,147 +279,149 @@ Muteferrika.prototype.shortcodes = function () {
  * @param {string} content The source content needs to be rendered which contains shortcodes
  * @returns {Promise<string>} Rendered output string
  */
-Muteferrika.prototype.render = async function (content) {
-  // validation
-  if (typeof content !== 'string') {
-    throw new TypeError(`"context" is expected to be a string but got: ${typeof content}`)
-  }
-  if (!content.trim()) return content
-
-  let output = content
-
-  // fetch matched tags
-  const {
-    tagMatches,
-    tagRegexp
-  } = _fetchTags.call(this, output)
-
-  // invoke all found shortcode callback functions
-  // and render them
-  for (let i = 0; i < tagMatches.length; i++) {
-    // parse the tag
-    const parsedTag =
-      _tagParser.call(this, tagMatches[i], output)
-
-    if (typeof parsedTag === 'string') {
-      output = parsedTag
-      continue
-    }
-
-    const {
-      shortcode,
-      fullMatch,
-      rawAttributes,
-      rawData
-    } = parsedTag
-
-    let data = rawData
-
+Muteferrika.prototype.render =
+  async function (content) {
     // validation
-    if (data) {
-      // render nested tags
-      const nestedTagMatches =
-        [...RegExp.prototype[Symbol.matchAll].call(tagRegexp, data)]
+    if (typeof content !== 'string') {
+      throw new TypeError(`"context" is expected to be a string but got: ${typeof content}`)
+    }
+    if (!content.trim()) return content
 
-      if (nestedTagMatches.length) {
-        data = await this.render(data)
+    let output = content
+
+    // fetch matched tags
+    const {
+      tagMatches,
+      tagRegexp
+    } = _fetchTags.call(this, output)
+
+    // invoke all found shortcode callback functions
+    // and render them
+    for (let i = 0; i < tagMatches.length; i++) {
+      // parse the tag
+      const parsedTag =
+        _tagParser.call(this, tagMatches[i], output)
+
+      if (typeof parsedTag === 'string') {
+        output = parsedTag
+        continue
       }
+
+      const {
+        shortcode,
+        fullMatch,
+        rawAttributes,
+        rawData
+      } = parsedTag
+
+      let data = rawData
+
+      // validation
+      if (data) {
+        // render nested tags
+        const nestedTagMatches =
+          [...RegExp.prototype[Symbol.matchAll].call(tagRegexp, data)]
+
+        if (nestedTagMatches.length) {
+          data = await this.render(data)
+        }
+      }
+
+      // fetch the tag attributes
+      const { attributes } =
+        _fetchTagAttributes.call(this, rawAttributes)
+
+      // invoke the shortcode callback and get the result
+      const scOutput =
+        await shortcode.callback(attributes, data)
+
+      // process final output
+      output =
+        _prepFinalOutput.call(
+          this,
+          fullMatch,
+          scOutput,
+          output
+        )
     }
 
-    // fetch the tag attributes
-    const { attributes } =
-      _fetchTagAttributes.call(this, rawAttributes)
-
-    // invoke the shortcode callback and get the result
-    const scOutput =
-      await shortcode.callback(attributes, data)
-
-    // process final output
-    output =
-      _prepFinalOutput.call(
-        this,
-        fullMatch,
-        scOutput,
-        output
-      )
+    // return the output
+    return output
   }
-
-  // return the output
-  return output
-}
 
 /**
  * Returns rendered output content
  * @param {string} content The source content needs to be rendered which contains shortcodes
  * @returns {string} Rendered output string
  */
-Muteferrika.prototype.renderSync = function (content) {
-  // validation
-  if (typeof content !== 'string') {
-    throw new TypeError(`"context" is expected to be a string but got: ${typeof content}`)
-  }
-  if (!content.trim()) return content
-
-  let output = content
-
-  // fetch matched tags
-  const {
-    tagMatches,
-    tagRegexp
-  } = _fetchTags.call(this, output)
-
-  // invoke all found shortcode callback functions
-  // and render them
-  for (let i = 0; i < tagMatches.length; i++) {
-    // parse the tag
-    const parsedTag =
-      _tagParser.call(this, tagMatches[i], output)
-
-    if (typeof parsedTag === 'string') {
-      output = parsedTag
-      continue
-    }
-
-    const {
-      shortcode,
-      fullMatch,
-      rawAttributes,
-      rawData
-    } = parsedTag
-
-    let data = rawData
-
+Muteferrika.prototype.renderSync =
+  function (content) {
     // validation
-    if (data) {
-      // render nested tags
-      const nestedTagMatches =
-        [...RegExp.prototype[Symbol.matchAll].call(tagRegexp, data)]
+    if (typeof content !== 'string') {
+      throw new TypeError(`"context" is expected to be a string but got: ${typeof content}`)
+    }
+    if (!content.trim()) return content
 
-      if (nestedTagMatches.length) {
-        data = this.renderSync(data)
+    let output = content
+
+    // fetch matched tags
+    const {
+      tagMatches,
+      tagRegexp
+    } = _fetchTags.call(this, output)
+
+    // invoke all found shortcode callback functions
+    // and render them
+    for (let i = 0; i < tagMatches.length; i++) {
+      // parse the tag
+      const parsedTag =
+        _tagParser.call(this, tagMatches[i], output)
+
+      if (typeof parsedTag === 'string') {
+        output = parsedTag
+        continue
       }
+
+      const {
+        shortcode,
+        fullMatch,
+        rawAttributes,
+        rawData
+      } = parsedTag
+
+      let data = rawData
+
+      // validation
+      if (data) {
+        // render nested tags
+        const nestedTagMatches =
+          [...RegExp.prototype[Symbol.matchAll].call(tagRegexp, data)]
+
+        if (nestedTagMatches.length) {
+          data = this.renderSync(data)
+        }
+      }
+
+      // fetch the tag attributes
+      const { attributes } =
+        _fetchTagAttributes.call(this, rawAttributes)
+
+      // invoke the shortcode callback and get the result
+      const scOutput =
+        shortcode.callback(attributes, data)
+
+      // process final output
+      output =
+        _prepFinalOutput.call(
+          this,
+          fullMatch,
+          scOutput,
+          output
+        )
     }
 
-    // fetch the tag attributes
-    const { attributes } =
-      _fetchTagAttributes.call(this, rawAttributes)
-
-    // invoke the shortcode callback and get the result
-    const scOutput =
-      shortcode.callback(attributes, data)
-
-    // process final output
-    output =
-      _prepFinalOutput.call(
-        this,
-        fullMatch,
-        scOutput,
-        output
-      )
+    // return the output
+    return output
   }
-
-  // return the output
-  return output
-}
 
 module.exports = Muteferrika
